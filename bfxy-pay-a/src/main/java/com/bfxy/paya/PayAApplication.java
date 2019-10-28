07ea26ad608b596ea5bf15696e74a35a99b6cbae
jishusc <jishusc@163.com>
2019-10-28 15:00:06 +0800

    添加工程bfxy-pay-a 以及 修复一些错误

---
+++ bfxy-pay-a/src/main/java/com/bfxy/paya/PayAApplication.java
@@ -0,0 +1,15 @@
+package com.bfxy.paya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayAApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(PayAApplication.class, args);
+		
+	}
+	
+}
