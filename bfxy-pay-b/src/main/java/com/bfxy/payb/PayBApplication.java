ca977af68bded296dd6e92a232cf9d5dc5042686
jishusc <jishusc@163.com>
2019-10-28 15:04:53 +0800

    添加项目bfxy-payb

---
+++ bfxy-pay-b/src/main/java/com/bfxy/payb/PayBApplication.java
@@ -0,0 +1,15 @@
+package com.bfxy.payb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayBApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(PayBApplication.class, args);
+		
+	}
+	
+}
