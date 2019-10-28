96e8a0c7866d5e28aee9668e8b4611cfcc56b47a
jishusc <jishusc@163.com>
2019-10-28 15:08:11 +0800

    添加项目bfxy-pkg

---
+++ bfxy-pkg/src/main/java/com/bfxy/pkg/PkgApplication.java
@@ -0,0 +1,15 @@
+package com.bfxy.pkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkgApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(PkgApplication.class, args);
+		
+	}
+	
+}
